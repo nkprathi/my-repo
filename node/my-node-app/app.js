78627703aad64c85d695f5d9a573c889ae24d176
Pratheeksha Naresh <pratheeksha1902@gmail.com>
2024-07-22 15:37:23 +0530

    changed some code in app.js

--- node/my-node-app/app.js
+++ node/my-node-app/app.js
@@ -6,7 +6,7 @@ const port = 3000;
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World! My Node First Program...');
+  res.send('Hello World! My First Program in Node...');
 });
 app.listen(3000, function () {
 console.log(`Server running at ${port}: http://${hostname}:${port}/`);
